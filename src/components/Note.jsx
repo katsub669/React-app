4bd5308c9d26b7ef8696974ed7a0da57c8fb6aab
katsub669 <oles.katsiuba@gmail.com>
2023-12-14 19:22:47 +0200

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note() {
+    return (
+        <div className='note'>
+            <h1>note test</h1>
+            <p> some text</p>
+        </div>
+    )
+}
+
+export default Note;
