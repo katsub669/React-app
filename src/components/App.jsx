add6079ba9d48d2e208183e320bf522ca7d20646
katsub669 <oles.katsiuba@gmail.com>
2023-12-14 21:56:29 +0200

    adding map func to Note.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import Header from './Header';
 import Note from './Note';
 import Footer from './Footer';
+import notes from '../notes';
+
+
+
 
 function App() {
     return (
     <div>
         <Header />
-        <Note />
+        {notes.map((item) => 
+            <Note 
+                key= {item.key}
+                title = {item.title}
+                content = {item.content}
+            />
+        )}
         <Footer />
     </div>
     )
