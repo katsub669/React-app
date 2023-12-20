236c04b3987427a9b37d7b124d5f18c2a176daaf
Oles <117779840+katsub669@users.noreply.github.com>
2023-12-21 00:38:40 +0200

    new comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,20 +6,22 @@ import CreateArea from './CreateArea';
 
 
 function App() {
+    //- constant that keeps track of the notes array.
     const [noteArray, setNoteArray] = useState([]);
-
+    //- getting ua new note from ./Note and adding it to an existing array.
     function addNote(formText) {
         setNoteArray((prevItems) => {
             return [...prevItems, formText];
           });
     }
-
+    
     function deleteNote(id) {
         setNoteArray((prevItems) => {
             return prevItems.filter((item, index) => id !== index)
         })
     }
-
+        // - Pass the addNote function to ./CreateArea
+        //- Take array and render seperate Note components for each item.
     return (
     <div>
         <Header />
