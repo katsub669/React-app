219a956da187ae1346939c4790f491655ca16a88
Oles <117779840+katsub669@users.noreply.github.com>
2023-12-21 00:40:57 +0200

    updated notes

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 
 function CreateArea(props) {
+    //- constant that keeps track of the title and content.
     const [formText, setFromText] = useState({
         title: '',
         content: ''
     });
-
+    //- cfunction that changes formText onChange in inputs.
     function handleChange(event) {
         const {value, name} = event.target;
         setFromText(prevValue => {
@@ -25,7 +26,9 @@ function CreateArea(props) {
             <textarea name="content" placeholder="Take a note..." rows="3" value={formText.content} onChange={handleChange} />
             <button 
                 onClick={(event) => {
+                    //passing formText to ./App to add the note to the array
                     props.addNote(formText);
+                    //clearing the formText
                     setFromText(
                         {
                             title: '',
